95d34dc2ab1ceaf4bf79484f51e8500198cf64b5
alu0100825503 <alu0100825503@ull.edu.es>
2016-04-13 16:49:49 +0000

    Corrección de index.ejs

--- public/csv.js
+++ public/csv.js
@@ -83,7 +83,7 @@ $(document).ready(() => {
    
     /* botones para rellenar el textarea */
     $('button.example').each( (_,y) => {
-     $(y).click( () => { dump(`public/${$(y).text()}.txt`); });
+     $(y).click( () => { dump(`${$(y).text()}.txt`); });
     });
 
     // Setup the drag and drop listeners.
