0dcf772b710d42263cc8c495fff5ef7f679e3978
alu0100825503 <alu0100825503@ull.edu.es>
2016-04-13 17:22:16 +0000

    Modificando csv.js

--- public/csv.js
+++ public/csv.js
@@ -87,6 +87,17 @@ $(document).ready(() => {
      $(y).click( () => { dump(`${$(y).text()}.txt`); });
     });
 
+    /* botón de selección en sistema de archivos */
+    $('input.inputfile').each( (_, y) => {
+      $(y).click( () => {
+        if (!y.files[0]) {
+          console.log("Select a file!");
+        } else {
+          dump(y.files[0]);
+        }
+      });
+    });
+
     // Setup the drag and drop listeners.
     //var dropZone = document.getElementsByClassName('drop_zone')[0];
     let dropZone = $('.drop_zone')[0];
