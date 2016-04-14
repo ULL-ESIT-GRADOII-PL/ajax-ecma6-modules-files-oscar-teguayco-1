915532d62ece0aa9c2bfec3d829918bc88c826ec
Óscar <alu0100705638@ull.edu.es>
2016-04-14 16:19:42 +0000

    Añadiendo algunas pruebas

--- test/calculate_test.js
+++ test/calculate_test.js
@@ -7,6 +7,25 @@ describe("CSV", function() {
         var input = '1, 2, 3';
         var r = calculate(input);
         expect(r[0].items.toString()).to.equal('1,2,3');
-     });   
+     });
+     
+     it("Deberia aceptar una entrada con comillas", function() {
+        var input = '"producto",     "precio"';
+        var r = calculate(input);
+        expect(r[0].items.toString()).to.equal('producto,precio');
+     });
+     
+     it("Deberia aceptar una entrada con parametros vacios", function(){
+        var input = '      ,    "4,3", ';
+        var r = calculate(input);
+        expect(r[0].items.toString()).to.equal('      ,4,3,');
+     });
+     
+     it("Deberia aceptar una entrada con 2 lineas", function(){
+        var input = '      ,    "4,3",  \n 1, 2, 3';
+        var r = calculate(input);
+        expect(r[0].items.toString()).to.equal('      ,4,3,');
+        expect(r[1].items.toString()).to.equal('1,2,3');
+     });
     }); 
  });
